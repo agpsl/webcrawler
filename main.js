95283221694992c707a6d5f4b151e86b21bcf39a
agpsl <agnaldo.silva.mg@gmail.com>
2024-02-22 15:04:21 -0300

    Add BASEURL in command line
    Implements an argument to be used as the base URL to start crawling

---
+++ main.js
@@ -0,0 +1,12 @@
+function main() {
+	if (process.argv.length != 3) {
+		console.log('Usage: npm run start BASEURL');
+		process.exit(1)
+	}
+
+	const baseURL = process.argv[2];
+
+	console.log(`Starting to crawl ${baseURL}`);
+}
+
+main()
